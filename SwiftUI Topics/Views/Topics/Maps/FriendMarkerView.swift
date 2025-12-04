717566525a993ed0f2d61bc1ea4a25b3ad6db7ec
csprasad <codealligator.csprasad@gmail.com>
2025-12-04 18:04:16 +0530

    update: Added new UI with Apple maps

---
+++ SwiftUI Topics/Views/Topics/Maps/FriendMarkerView.swift
@@ -0,0 +1,29 @@
+//
+//  FriendMarkerView.swift
+//  SwiftUI Topics
+//
+//  Created by codeAlligator on 03/12/25.
+//
+
+
+import SwiftUI
+
+struct FriendMarkerView: View {
+    let friend: Friend
+    
+    var body: some View {
+        VStack(spacing: 4) {
+            ZStack {
+                Circle()
+                    .fill(.background)
+                    .frame(width: 46, height: 46)
+                    .shadow(radius: 4)
+                
+                FriendAvatar(url: friend.imageURL, size: 40)
+            }
+            
+            RoundedRectangle(cornerRadius: 8)
+                .frame(width: 4, height: 12)
+        }
+    }
+}
